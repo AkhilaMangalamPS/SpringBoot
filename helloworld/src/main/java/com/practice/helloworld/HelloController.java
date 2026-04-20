331ece0602cec4f07c9c622b2f5b0d7f8f978e0c
AkhilaMangalamPS <akhila.mangalam@batonsystems.com>
2026-04-20 23:02:14 +0530

    Update HelloController.java

--- helloworld/src/main/java/com/practice/helloworld/HelloController.java
+++ helloworld/src/main/java/com/practice/helloworld/HelloController.java
@@ -1,4 +1,4 @@
-package com.practice.helloworld;
+ package com.practice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,28 +18,28 @@ public class HelloController {
     }
 
 
-//    @GetMapping("/hello")
-//    public String sayHello(){
-//        return "welcome Akhila!";
-//    }
-//
-//    @GetMapping("/bye")
-//    public String sayGoodBye(){
-//        return "GoodBye Akhila!!";
-//    }
-//
-//    @GetMapping("/name")
-//    public String getName(@RequestParam String user){
-//        return "My name is "+ user;
-//    }
-//
-//    @GetMapping("/newname/{n}")
-//    public String name(@PathVariable String n){
-//        return "My name is "+ n;
-//    }
-//
-//    @GetMapping("/nameage/{age}")
-//    public String info(@PathVariable int age,@RequestParam String name){
-//        return "Name :" + name + "Age: " + age;
-//    }
+   @GetMapping("/hello")
+   public String sayHello(){
+       return "welcome Akhila!";
+   }
+
+   @GetMapping("/bye")
+   public String sayGoodBye(){
+       return "GoodBye Akhila!!";
+   }
+
+   @GetMapping("/name")
+   public String getName(@RequestParam String user){
+       return "My name is "+ user;
+   }
+
+   @GetMapping("/newname/{n}")
+   public String name(@PathVariable String n){
+       return "My name is "+ n;
+   }
+
+   @GetMapping("/nameage/{age}")
+   public String info(@PathVariable int age,@RequestParam String name){
+       return "Name :" + name + "Age: " + age;
+   }
 }
